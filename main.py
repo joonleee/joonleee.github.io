b69f9e9866e4268eb5c5c25029297ce03232c566
joonleee <88965328+joonleee@users.noreply.github.com>
2023-01-30 18:41:45 +0900

    Changed folder hierarchy

--- main.py
+++ main.py
@@ -5,7 +5,12 @@ app = Flask(__name__)
 
 @app.route("/") # base url = run index()
 def index():
-    return "It's a web app!"
+    return "Welcome!"
+
+@app.route("/nnderaid")
+def nnde():
+    return "nnde!"
+
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
